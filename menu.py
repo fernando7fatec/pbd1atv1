5fd0a1b0ecebf92379b353a4836ca930653df658
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-09-10 09:57:20 -0300

    feat: implemented division func

--- menu.py
+++ menu.py
@@ -1,8 +1,11 @@
 def soma(a,b):
     return a + b
-    
+
 def multiplica(a,b):
     return a * b
+
+def divisao(a,b):
+    return a/b
 
 def menu():
    m = "\n1.Somar\n2.Subtrair\n3.Multiplicar\n4.Dividir\n0.Sair\n"
